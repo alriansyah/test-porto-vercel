77132b1818dca434ac64491d07b2cba7433a83bd
alriansyah <alriansyah1218@gmail.com>
2023-11-18 22:54:24 +0700

    manage SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
